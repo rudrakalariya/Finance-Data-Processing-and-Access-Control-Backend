862df2064f4b2d9e25ad778cced8909756a48800
Rudra Kalariya <rudrakalariya1@gmail.com>
2026-04-05 22:18:33 +0530

    Updated SecurityConfig

--- src/main/java/com/finance/security/SecurityConfig.java
+++ src/main/java/com/finance/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                         "/v3/api-docs/**",
                         "/swagger-ui/**",
                         "/swagger-ui.html",
-                        "/h2-console/**"
+                        "/h2-console/**",
                         "/webjars/**"
                 ).permitAll()
                 .anyRequest().authenticated()
